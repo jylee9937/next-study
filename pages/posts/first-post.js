95a27243c6e5c3804c1460173643f8b27711d5f4
jylee9937 <lkj49864@ajou.ac.kr>
2023-01-23 21:30:33 +0900

    :construction: Layouts-styling

--- pages/posts/first-post.js
+++ pages/posts/first-post.js
@@ -1,3 +1,11 @@
+import Layout from "../../components/Layout";
+import Head from "next/head";
+
 export default function FirstPost() {
-	return <h1>첫번째 포스트</h1>
+	return <Layout>
+		<Head>
+			<title>첫번째 포스트</title>
+		</Head>
+		<h1>첫번째 포스트</h1>
+	</Layout>
 }
